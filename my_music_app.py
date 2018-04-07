10c8ee937c0b39bd46e06b03489be6200039ae94
Ekaterina Tyurina <tyurina.katty@yandex.ru>
2018-04-07 17:50:27 +0100

    Change host

--- my_music_app.py
+++ my_music_app.py
@@ -111,4 +111,4 @@ def search_artist():
 
 app.secret_key = os.urandom(30)
 
-app.run(port=int(os.getenv("PORT", 8888)), debug=True)
+app.run(port=int(os.getenv("PORT", 8888)), host="0.0.0.0", debug=True)
